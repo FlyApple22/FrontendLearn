644e63fbf04f1b3aa85bdb25ec1b831bbfe013c9
zhaotao6 <1363415964@qq.com>
2025-07-06 23:33:51 +0800

    ommit
    js更新

--- Javascript/foundation2/practice.js
+++ Javascript/foundation2/practice.js
@@ -35,13 +35,39 @@ if (numbers > 0) {
   console.log('负数');
 }
 // 7.使用 switch 判断红绿黄灯
-
+let light = 2;
+switch (light) {
+  case 1:
+    console.log('red');
+    break;
+  case 2:
+    console.log('green');
+    break;
+  case 3:
+    console.log('yellow');
+    break;
+  default:
+    console.log('error');
+    break;
+}
 // 8.用 for 循环打印 1~5
-
-// 9.使用 while 计算从 1 加到 5 的和
-
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+// 9.使用 while 计算从 1 加到 3 的和
+let i = 1;
+let sum = 0;
+while (i <= 3) {
+  sum += i;
+  i++;
+}
 // 10.使用 for 循环跳过数字 3（continue）
-
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+  if (i === 3) {
+    continue;
+  }
+}
 // 11.打印九九乘法表
 
 // 一、你正在设计一个简单的报名系统，只有年龄在 18 到 60 岁之间，并且已经 填写了电话号码的用户才允许报名成功
@@ -63,7 +89,17 @@ if (user.age > 18 && user.age < 60 && user.phone !== '') {
 //- 60~79 是 C
 //- 60 以下是 D
 let scores = [92, 85, 73, 59, 66];
-
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] >= 90) {
+    console.log('A');
+  } else if (scores[i] >= 80) {
+    console.log('B');
+  } else if (scores[i] >= 60) {
+    console.log('C');
+  } else {
+    console.log('D');
+  }
+}
 // 三、现在有一个购物车，里面是若干商品对象，每个商品都有 `name`、`price` 和 `isMemberOnly`（是否会员专属）。
 // 请打印出所有可以给非会员顾客购买的、价格低于 100 的商品名称。
 let cart = [
@@ -73,6 +109,11 @@ let cart = [
   { name: '洗发水', price: 99, isMemberOnly: false },
 ];
 
+for (let i = 0; i < cart.length; i++) {
+  if (cart[i].price < 100 && !cart[i].isMemberOnly) {
+    console.log(cart[i].name);
+  }
+}
 // 四、课程成绩表
 // 一个二维数组保存了 3 个学生的成绩（语文、数学、英语）。请你使用 for 循环输出每位学生的总分。
 let grades = [
@@ -97,7 +138,12 @@ let users = {
 // 跳过没签到的学生，只输出签到成功的。
 let statusList = [true, false, true, true, false];
 let studentName = ['小明', '小红', '小刚', '小华', '小丽'];
-
+for (let i = 0; i < statusList.length; i++) {
+  if (statusList[i] === false) {
+    continue;
+  }
+  console.log(studentName[i]);
+}
 // 七、商品搜索
 // 用户输入一个商品名，程序遍历商品数组，找到商品就显示信息并停止搜索
 let products = [
@@ -106,6 +152,14 @@ let products = [
   { name: '肥皂', price: 5 },
 ];
 
+let searchName = '洗发水';
+
+for (let i = 0; i < products.length; i++) {
+  if (products[i].name === searchName) {
+    console.log(products[i]);
+    break;
+  }
+}
 // 八、打卡系统
 // 你设计一个打卡系统，记录员工每天是否打卡成功。
 // 每个员工是一个对象属性，值为一个包含 7 天打卡状态的数组（布尔值）。请判断并打印出那些打卡次数少于 5 次的人。
