770650c1dfde6b347ea45da719a1517e11390eed
Jett191 <114122471+Jett191@users.noreply.github.com>
2025-07-19 19:05:46 +0800

    arrayPractice

---
+++ Javascript/foundation2/practice/practice8.js
@@ -0,0 +1,34 @@
+// 1.用户昵称长度验证
+// 请你编写一个函数 isValidName(name) 用于判断一个用户的昵称是否合法。合法昵称要求：去除前后空格后长度必须在 2 到 8 个字符之间。 .trim() .length
+
+// 2.统计字母 E 出现次数
+//  有一段文本 "Excellent effort elevates everyone."，请编写代码统计字母  "e"（不区分大小写）出现的次数。 .toLowerCase() 、.split()
+
+// 3.邮箱后缀校验
+// 用户提交了邮箱地址，请你判断这个邮箱是否以  .com  结尾。
+
+// 4.提取用户名首字母并大写
+// 输入用户名，如 " alice "，请返回首字母的大写形式。例如返回 "A"
+
+let name = '  alice  ';
+
+// 5.分割关键词为数组
+// 一个输入框里填写了关键词 "apple, banana, orange"，请你将它拆分成数组。
+
+// 6.输出长度超过5的单词
+// 给定一句话 "JavaScript is very powerful and interesting" 请你打印出所有长度大于 5 的单词
+let sentence = 'JavaScript is very powerful and interesting';
+
+// 7.替换敏感词
+// 网站评论系统中，有人留言 "I hate this product!"，请你把 "hate" 替换成 "dislike"。
+let comment = 'I hate this product!';
+
+// 8.将数组中每个字符串变大写
+// 有一个字符串数组 ["red", "green", "blue"]，请你把它全部转换为大写字母形式。
+
+// 9.过滤空字符串
+// 有一个用户输入的数组 ，请你过滤掉空字符串和仅包含空格的字符串。
+let arr = ['apple', ' ', 'banana', '', 'peach'];
+
+// 10.统计每个单词长度
+// 编写一个函数 wordLengths(sentence)，传入一句英文句子，如 "I love JavaScript"，返回每个单词的长度数组。
---
+++ Javascript/foundation2/practice/practice9.js
@@ -0,0 +1,36 @@
+// 1.提取不及格成绩
+// 定义一个函数  getFailed(scores)，返回所有小于 60 的成绩数组。
+let firstScores = [85, 59, 40, 100, 60];
+
+// 2.所有分数+5分
+// 创建函数  addBonus(scores) ，将所有成绩都加 5 分并返回新的数组（不改变原数组）
+let secondScores = [70, 80, 90];
+
+// 3.找出第一个不及格的人名
+// 有一组学生对象，每个包含  name  和  score 。编写函数  findFirstFail(students)  找出第一个不及格（<60）学生的姓名。
+let students = [
+  { name: 'Amy', score: 80 },
+  { name: 'Bob', score: 55 },
+  { name: 'Cindy', score: 90 },
+];
+
+// 4.自定义排序分数从高到低
+// 写一个函数  sortDesc(arr) ，将分数从高到低排序。
+let thirdScores = [70, 85, 60, 90];
+
+// 5.打分等级
+// 请创建函数  gradeList(scores) ，将一组分数映射为 A/B/C/D/F。
+// | 分数范围 | 等级 |
+// | 90-100   | A    |
+// | 80-89    | B    |
+// | 70-79    | C    |
+// | 60-69    | D    |
+// | < 60     | F    |
+let fifthScores = [95, 83, 72, 50];
+
+// 6.过滤出字符串中的数字
+// 写一个函数  onlyNumbers(arr) ，从混合数组中提取出所有数字。
+let mixArray = [1, 'hello', 3, true, '5', 9];
+
+// 7.打乱顺序
+// 写一个函数  shuffle(arr) ，返回数组的乱序版本   提示Math.random()生成 0到1的随机数
