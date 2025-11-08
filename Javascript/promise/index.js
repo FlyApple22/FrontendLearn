97136eee121e88c71825e9576dcaa509c57474c9
Jett191 <114122471+Jett191@users.noreply.github.com>
2025-11-08 10:16:44 +0800

    demo

--- Javascript/promise/index.js
+++ Javascript/promise/index.js
@@ -1,27 +1,23 @@
-// ✅ PUT：整体更新文章内容（所有字段都会被覆盖）
-async function updatePost() {
-  try {
-    const response = await fetch(
-      'https://jsonplaceholder.typicode.com/posts/1',
-      {
-        method: 'PUT', // ✅ PUT = 整体覆盖
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title: 'New Title',
-          body: 'New Content',
-          userId: 1,
-        }),
-      }
-    );
-
-    const result = await response.json();
-
-    console.log('✅ 更新成功！新的标题：', result.title);
-  } catch (err) {
-    console.error('❌ 更新文章失败：', err);
-  }
+// 定义函数
+function mokeRequest(callback) {
+  console.log('开始请求数据');
+  let data = [1, 1];
+  callback(data);
 }
 
-updatePost();
+function addNumbers(arr) {
+  const sum = arr[0] + arr[1];
+  console.log(`总数是 ${sum}`);
+}
+
+function jianNumbers(arr) {
+  const sum = arr[0] - arr[1];
+  console.log(`总数是 ${sum}`);
+}
+
+// 执行函数
+// 加法
+mokeRequest(addNumbers);
+
+// 减法
+mokeRequest(jianNumbers);
