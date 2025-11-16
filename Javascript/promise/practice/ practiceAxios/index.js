8766b9e3c70ff5617359992d900d3a828a9a1a55
zhaotao6 <1363415964@qq.com>
2025-11-16 11:40:29 +0800

    222

--- Javascript/promise/practice/ practiceAxios/index.js
+++ Javascript/promise/practice/ practiceAxios/index.js
@@ -102,23 +102,23 @@ import axios from 'axios';
 // }
 // change(1);
 // 把 ID=2 的文章，只更新 title: "Patched Title"
-async function change(id) {
-  try {
-    const { data } = await axios.put(
-      `https://jsonplaceholder.typicode.com/posts/${id}`,
-      { title: 'Patched Title' },
-      {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-change(2);
+// async function change(id) {
+//   try {
+//     const { data } = await axios.put(
+//       `https://jsonplaceholder.typicode.com/posts/${id}`,
+//       { title: 'Patched Title' },
+//       {
+//         headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//         },
+//       }
+//     );
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// change(2);
 
 // async function change(id) {
 //   try {
@@ -141,3 +141,93 @@ change(2);
 //   }
 // }
 // change(2);
+
+// async function a(email) {
+//   try {
+//     const { data } = await axios.post(
+//       `https://api.apiopen.top/api/sendVerificationCode`,
+//       { mail: email }
+//     );
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// a('1363415964@qq.com');
+
+// async function b() {
+//   try {
+//     const { data } = await axios.post(`https://api.apiopen.top/api/register`, {
+//       account: '1363415964@qq.com',
+//       code: '138805',
+//       password: '123456',
+//     });
+//     if (data.code === 200) {
+//       console.log('请求成功');
+//     } else {
+//       console.log('请求失败');
+//     }
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// b();
+
+// 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE5MzYsImlkIjoxOTM2LCJjcmVhdGVkQXQiOiIyMDI1LTExLTE1IDEwOjU3OjAwIiwidXBkYXRlZEF0IjoiMjAyNS0xMS0xNSAxMDo1NzowMCIsImRlbGV0ZWRBdCI6bnVsbCwiYWNjb3VudCI6IjEzNjM0MTU5NjRAcXEuY29tIiwibGV2ZWwiOjAsImV4cCI6MTc2Mzc4MDIyMCwiaXNzIjoiYXBpX29wZW4iLCJuYmYiOjE3NjMxNzQ0MjB9._nyE66ggU7R_q24ppnBeH_C3ZR_zr0KNJjhD0ezWi5A'
+
+// async function c() {
+//   try {
+//     const { data } = await axios.post('https://api.apiopen.top/api/login', {
+//       account: '1363415964@qq.com',
+//       password: '123456',
+//     });
+
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//     return null;
+//   }
+// }
+
+// let data = await c();
+// console.log(data);
+let token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE5MzYsImlkIjoxOTM2LCJjcmVhdGVkQXQiOiIyMDI1LTExLTE1IDEwOjU3OjAwIiwidXBkYXRlZEF0IjoiMjAyNS0xMS0xNSAxMDo1NzowMCIsImRlbGV0ZWRBdCI6bnVsbCwiYWNjb3VudCI6IjEzNjM0MTU5NjRAcXEuY29tIiwibGV2ZWwiOjAsImV4cCI6MTc2Mzc4Mzc3NSwiaXNzIjoiYXBpX29wZW4iLCJuYmYiOjE3NjMxNzc5NzV9.zQJjFUT3zjoMtZmTgzaI0tk6kdPAlcn5Oba47GmW6NQ';
+// console.log(token);
+
+// async function a() {
+//   try {
+//     const { data } = await axios.post(
+//       `https://api.apiopen.top/api/publishDynamic`,
+//       {
+//         images: [
+//           'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Sunset_2007-1.jpg/500px-Sunset_2007-1.jpg',
+//           'https://upload.wikimedia.org/wikipedia/commons/8/87/Red_sunset.jpg',
+//         ],
+//         text: '今天心情真好!',
+//       },
+//       { headers: { token: token } }
+//     );
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// a();
+
+async function a() {
+  try {
+    const { data } = await axios.get(
+      `https://api.apiopen.top/api/getPersonDynamicList`,
+      { params: { page: 0, size: 10 }, headers: { token: token } }
+    );
+    console.log(data);
+    console.log(data.result.list);
+  } catch (error) {
+    console.log(error);
+  }
+}
+a();
