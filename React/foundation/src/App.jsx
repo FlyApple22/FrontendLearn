6a5d04a4891377a71ac0d75d43cdea2c28a5e789
Zhaotao6 <1363415964@qq.com>
2026-03-21 11:31:00 +0800

    ✨ feat(test): test

--- React/foundation/src/App.jsx
+++ React/foundation/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 //state 组件记忆 usestate创建state
 function App() {
   //写js的地方
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   function add() {
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
+    setCount((prev) => prev + 1)
   }
-  const [name, setName] = useState("");
+  const [name, setName] = useState('')
 
   //写html的地方
   return (
@@ -18,7 +16,7 @@ function App() {
       <h1>{count}</h1>
       <input value={name} onChange={(e) => setName(e.target.value)} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
