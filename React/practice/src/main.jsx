fbcb02501242ee55fb85939657b7cd3f36113c63
Jett191 <114122471+Jett191@users.noreply.github.com>
2026-03-21 11:26:02 +0800

    🎉 init(all): init

---
+++ React/practice/src/main.jsx
@@ -0,0 +1,19 @@
+import './index.css'
+
+// import StepCounter from './components/01StepCounter'
+
+export default function App() {
+  return (
+    <div className='grid'>
+      {/* <StepCounter />
+      <TimerLike />
+      <LoginValidator />
+      <FontClamp />
+      <ThemePreview />
+      <MiniCart />
+      <TodoLite />
+      <FilterPanel />
+      <ProfileSubmit /> */}
+    </div>
+  )
+}
