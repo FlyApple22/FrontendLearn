649752e081519149370e811144e6d982aa7f07b1
Jett191 <114122471+Jett191@users.noreply.github.com>
2026-04-18 10:44:35 +0800

    ✨ feat(practice): math\&Date

---
+++ Javascript/Data&Math/index.js
@@ -0,0 +1,21 @@
+// 四舍五入
+console.log(Math.round(4.4));
+console.log(Math.round(4.5));
+
+// 向上 & 向下 取整
+console.log(Math.floor(4.9));
+console.log(Math.floor(4.1));
+console.log(Math.ceil(4.1));
+
+// 去小数
+console.log(Math.trunc(4.1));
+
+// 最大值 & 最小值
+console.log(Math.max(3, 5, 7, 5, 4, 3));
+console.log(Math.min());
+
+// 绝对值
+console.log(Math.abs(-1));
+
+//  随机数
+console.log(Math.random());
